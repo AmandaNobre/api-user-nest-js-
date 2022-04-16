1436d39396ac6c0682de6b087933a7e4d5386f6b
Amanda Nobre <34144662+AmandaNobre@users.noreply.github.com>
2022-04-16 16:14:35 -0300

    crud de usuários

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/ussers'),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
